ceb93ad0d884a32bd67a8c2ec58924cf47bf458a
realChakrawarti <anupam.chakrawarti@outlook.com>
2023-02-27 11:41:25 +0530

    feat: add express js, routing & parse data from client

--- app.js
+++ app.js
@@ -1,62 +1,31 @@
-const http = require("http");
-const fs = require("fs");
+import express from "express";
+import bodyParser from "body-parser";
 
-// Callback function
-function reqListener(req, res) {
-  const { url, method } = req;
-  res.setHeader("Content-Type", "text/html");
+const PORT = 3000;
+const app = express();
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Node.js Server Demo</title></head>");
-    res.write(
-      '<body style="text-align:center;"><h2>Welcome to homepage server</h2></body>'
-    );
-    res.write("</html>");
-    res.end();
-  }
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  if (url === "/feedback") {
-    res.write("<html>");
-    res.write("<head><title>Share feedback</title></head>");
-    res.write(
-      '<body style="text-align:center;"><h2>Feedback form</h2><hr/><form style="display: flex;flex-direction: column;align-items: center;gap: 1rem;" action="/feedback-received" method="POST"><textarea placeholder="Enter your feedback" rows="4" cols="80" name="feedback"></textarea><button type="submit">Send feedback</button></form></body>'
-    );
-    res.write("</html>");
-    res.end();
-  }
+app.use("/users", (req, res, next) => {
+  res.send("<h3>Hello from Users page</h3>");
+});
 
-  if (url === "/feedback-received") {
-    if (method === "POST") {
-      // Listens to data received from client in buffer.
-      // Callback will work on every data piece
-      const bufferData = [];
-      req.on("data", (chunk) => {
-        bufferData.push(chunk);
-      });
-      req.on("end", () => {
-        const fileName = Date.now() + "";
-        const parsedData = Buffer.concat(bufferData).toString();
-        const stringData = parsedData.split("=")[1];
-        fs.writeFile(fileName + ".md", stringData, (err) => {
-          res.write("<html>");
-          res.write("<head><title>Feedback received</title></head>");
-          res.write(
-            '<body style="text-align:center;"><h3>Thank you for your submission!</h3></body>'
-          );
-          res.write("</html>");
-          res.end();
-        });
-      });
-    } else {
-      res.writeHead(302, { Location: "/feedback" });
-      res.end();
-    }
-  }
-}
+app.use("/add-user", (req, res, next) => {
+  res.send(
+    "<h3>Check username</h3><hr/><form method='POST' action='/check-user'><input type='text' name='username' placeholder='Check username'/><button type='submit'>Check</button></form>"
+  );
+});
 
-const server = http.createServer(reqListener);
+app.post("/check-user", (req, res, next) => {
+  console.log("Checking username", req.body);
+  res.send(`<h3>${req.body.username} is available! 🎉</h3>`);
+});
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
+// By default path is "/"
+app.use((req, res, next) => {
+  res.send("<h3>Hello from ExpressJS</h3>");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}...`);
 });
